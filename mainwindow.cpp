f599e31624bf567862533e60eca7b723bcc86c60
DenisKudryavcev <Elenakud152@gmail.com>
2023-06-06 15:33:28 +0300

    First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,195 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("Task 3");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//Кнопки настройки
+void MainWindow::on_radioButtonScroll_clicked(bool checked)
+{
+    if (checked){
+        ui->groupBoxScrollBars->setEnabled(true);
+        ui->groupBoxSliders->setEnabled(false);
+    }
+}
+
+void MainWindow::on_radioButtonSpin_clicked(bool checked)
+{
+    if (checked){
+        ui->groupBoxScrollBars->setEnabled(false);
+        ui->groupBoxSliders->setEnabled(false);
+    }
+}
+
+void MainWindow::on_radioButtonSlider_clicked(bool checked)
+{
+    if (checked){
+
+        ui->groupBoxScrollBars->setEnabled(false);
+        ui->groupBoxSliders->setEnabled(true);
+    }
+
+}
+
+void MainWindow::on_radioButtonDial_clicked(bool checked)
+{
+    if (checked){
+        ui->groupBoxScrollBars->setEnabled(false);
+        ui->groupBoxSliders->setEnabled(false);
+    }
+
+}
+
+
+//Запись диапазона в label
+void MainWindow::on_scrollBarPozitive_valueChanged(int value)
+{
+    ui->label_2->setText(QString::number(value));
+}
+
+
+void MainWindow::on_scrollBarNigative_valueChanged(int value)
+{
+    ui->label->setText(QString::number(value));
+}
+
+void MainWindow::on_spinBoxPozitive_valueChanged(int arg1)
+{
+    ui->label_2->setText(QString::number(arg1));
+}
+
+
+void MainWindow::on_spinBoxNigative_valueChanged(int arg1)
+{
+    ui->label->setText(QString::number(arg1));
+}
+
+
+void MainWindow::on_sliderNegative_valueChanged(int value)
+{
+     ui->label_2->setText(QString::number(value));
+}
+
+
+void MainWindow::on_sliderPozitive_valueChanged(int value)
+{
+     ui->label->setText(QString::number(value));
+}
+
+
+void MainWindow::on_dialPositive_valueChanged(int value)
+{
+     ui->label_2->setText(QString::number(value));
+}
+
+
+void MainWindow::on_dialNigative_valueChanged(int value)
+{
+     ui->label->setText(QString::number(value));
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    //Проверка на ошибки длинны массива
+    QString size = ui->lineEditSize->text();
+    try {
+        n = size.toInt();
+        mas = new int [n];
+        }
+
+    catch (...) {
+        ui->textEdit->setText("Меняйте размерность массива!");
+        return;
+    }
+
+
+
+    //Инициализация диапазона
+    if (ui->radioButtonScroll->isChecked() == true){
+        pozitive = ui->scrollBarPozitive->value();
+        negative = ui->scrollBarNigative->value();
+    }
+    else if (ui->radioButtonSlider->isChecked() == true){
+        pozitive = ui->sliderPozitive->value();
+        negative = ui->sliderNegative->value();
+    }
+    for (int i = 0; i < n; i++) {
+        mas[i] = randomBetween(pozitive, negative);
+    }
+    QString line;
+    for (int i = 0; i < n; i++ ) {
+        line += QString::number(mas[i]) + ' ';
+    }
+
+    QString count;
+    if (ui->radioButtonTaskA->isChecked() == true){
+        int sumA = 0;
+        for (int i = 0; i < n; i+=2 ) {
+          sumA += mas[i];
+        }
+        QString A = QString::number(sumA);
+        count = line + "\n\n" + A;
+
+    }
+     else if (ui->radioButtonTaskB->isChecked() == true){
+        int sumB = 0;
+        bool flag = true;
+        for (int i = 0; i < n ; i++ ) {
+            if (mas[i] < 0){
+                if (i != n - 1)
+                    flag = false;
+                for (int j = i + 1; j < n ; j++ ) {
+                    if (mas[j] >= 0){
+                        sumB += mas[j];
+                    }
+                    else
+                        break;
+                    if (j == n - 1)
+                        flag = true;
+                }
+                break;
+            }
+
+        }
+        if(flag == false){
+            QString B = QString::number(sumB);
+            count = line + "\n\n" + B;
+        }
+        else{
+            count = line + "\n" + "Нет отрицательных чисел или нет второго отрицательного числа!";
+        }
+       }
+    else{
+        count = "Выберите задание";
+    }
+    delete[] mas;
+
+    ui->textEdit->setText(count);
+    if (ui->checkBoxIFiles->isChecked()){
+        writeFile(count);
+    }
+
+}
+
+void MainWindow::writeFile(QString line){
+    QFile file("file.txt");
+    file.open(QIODevice::WriteOnly);
+    QByteArray ba = line.toLocal8Bit();
+    const char *count = ba.data();
+    file.write(count);
+    file.close();
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_radioButtonScroll_clicked(bool checked);
+
+    void on_radioButtonSpin_clicked(bool checked);
+
+    void on_radioButtonSlider_clicked(bool checked);
+
+    void on_radioButtonDial_clicked(bool checked);
+
+    void on_scrollBarPozitive_valueChanged(int value);
+
+    void on_scrollBarNigative_valueChanged(int value);
+
+    void on_spinBoxPozitive_valueChanged(int arg1);
+
+    void on_spinBoxNigative_valueChanged(int arg1);
+
+    void on_sliderNegative_valueChanged(int value);
+
+    void on_sliderPozitive_valueChanged(int value);
+
+    void on_dialPositive_valueChanged(int value);
+
+    void on_dialNigative_valueChanged(int value);
+
+    void on_pushButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    int *mas;
+    int n;
+    int pozitive, negative;
+    static int randomBetween(int max1, int min1){
+        return (qrand() % ((max1 + 1) - min1) + min1);
+    }
+    void writeFile(QString line);
+};
+#endif // MAINWINDOW_H
